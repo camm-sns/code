48668f1add33e5461045f94a23c7ee6a5c95a0de
Doucet, Mathieu <doucetm@ornl.gov>
2013-02-28 15:43:36 -0500

    Added dummy AMQ consumer for Kepler

--- dakota/README.md
+++ dakota/README.md
@@ -1,1 +1,4 @@
 This directory is meant to contain Dakota-specific code and configuration for CAMM
+
+# Pre-requisites
+	 - stomp.py should be installed to enable ActiveMQ communication (http://code.google.com/p/stomppy/)
---
+++ kepler/activemq/setup.py
@@ -0,0 +1,21 @@
+"""
+    Install the ActiveMQ module for Kepler
+    
+    Requires stomp.py: run easy_install stomp.py
+    
+    Installs the kepler_consumer script, which can be called
+    to start an ActiveMQ consumer for Kepler
+    
+"""
+from setuptools import setup
+import shutil
+
+setup(
+    name         = "amq_kepler",
+    version      = "1.0",
+    description  = "ActiveMQ consumer for Kepler",
+    author       = "Oak Ridge National Laboratory",
+    package_dir  = {'amq_kepler': '.'},
+    packages     = ['amq_kepler'],
+    entry_points = {'console_scripts':["kepler_consumer = amq_kepler.kepler_consumer:run",]},
+    )   
