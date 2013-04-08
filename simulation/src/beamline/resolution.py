b1ff866e3867a24ccb12181695154960392c1e52
Jose Borreguero <borreguero@gmail.com>
2013-04-08 18:30:23 -0400

    Refs #4 Fixed some bugs

--- simulation/src/beamline/resolution.py
+++ simulation/src/beamline/resolution.py
@@ -5,6 +5,7 @@ Create on Apr 8, 2013
 
 @author: jmborr
 '''
+from pdb import set_trace as trace # uncomment only for debugging purposes
 
 def elasticLineLowTemp(insqe, outres=None):
   '''Produces a resolution function based on the quasi-elastic signal at low temperature
@@ -16,7 +17,7 @@ def elasticLineLowTemp(insqe, outres=None):
   Returns:
     mantid workspace containing Res(Q,E)
   '''
-  from mantid.simpleapi import (LoadNexus, ExtractSingleSpectrum, AppendSpectra, ScaleX, SaveNexus)
+  from mantid.simpleapi import (LoadNexus, ExtractSingleSpectrum, NormaliseToUnity, AppendSpectra, ScaleX, SaveNexus)
   wse=LoadNexus(Filename=insqe,OutputWorkspace='insqe')
   for iw in range(wse.getNumberHistograms()):
     iname='insqe'+str(iw)
@@ -37,10 +38,14 @@ if __name__ == "__main__":
   p.add_argument('service', help='name of the service to invoke')
   p.add_argument('-explain', action='store_true', help='print message explaining the arguments to pass for the particular service')
   if Set(['-h', '-help', '--help']).intersection(Set(sys.argv)): args=p.parse_args() # check if help message is requested
-
   if 'elasticLineLowTemp' in sys.argv:
     p.description='Produces a resolution function based on the quasi-elastic signal at low temperature'
     for action in p._actions:
       if action.dest=='service': action.help='substitue "service" with "elasticLineLowTemp"' # update help message
     p.add_argument('--insqe',help=' Nexus file containing S(Q,E) at low temperature. One spectrum per Q-value.')
     p.add_argument('--outres',help='output Nexus file containing Res(Q,E). Each spectrum is separately applied the Mantid::NormaliseToUnity algorithm.')
+    if '-explain' in sys.argv:
+      p.parse_args(args=('-h',))
+    else:
+      args=p.parse_args()
+      elasticLineLowTemp(args.insqe, outres=args.outres)
