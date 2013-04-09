bdb8c9dcf2f9c8e62f27a2f8ba3da0f1cd39553c
Jose Borreguero <borreguero@gmail.com>
2013-04-09 15:14:48 -0400

    Fixed help message in resolution.py

--- simulation/src/beamline/resolution.py
+++ simulation/src/beamline/resolution.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
   import argparse
   import sys
   from sets import Set
-  p=argparse.ArgumentParser(description='Provider for services involving the production of a resolution function. Available services are: None')
+  p=argparse.ArgumentParser(description='Provider for services involving the production of a resolution function. Available services are: elasticLineLowTemp')
   p.add_argument('service', help='name of the service to invoke')
   p.add_argument('-explain', action='store_true', help='print message explaining the arguments to pass for the particular service')
   if Set(['-h', '-help', '--help']).intersection(Set(sys.argv)): args=p.parse_args() # check if help message is requested
