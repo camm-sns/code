6752d7a3068e63ca170c0b4492d1090c66eca1df
Jose Borreguero <borreguero@gmail.com>
2013-04-15 14:08:13 -0400

    Refs #10 modelBEC1Q to account for 1/Q dependence of the empty can

--- simulation/src/beamline/assemblemodel.py
+++ simulation/src/beamline/assemblemodel.py
@@ -47,6 +47,48 @@ def modelBEC(model, resolution, convolved, qvalues, assembled, expdata=None, cos
     DakotaChiSquared(DataFile=assembled,CalculatedFile=expdata,OutputFile=costfile)
   return wsc
 
+def modelBEC1Q(model, resolution, convolved, qvalues, assembled, expdata=None, costfile=None):
+  """Assemble the Background, Elastic line and Convolution of the resolution with the simulated S(Q,E)
+  This is a hard-coded model consisting of a linear background, and elastic line, and a convolution:
+    b0+b1*E  +  1/Q*e0**Elastic(E)  +  c0*Resolution(E)xSimulated(Q,E)
+    We load Resolution(E)xSimulated(Q,E) as Convolved(Q,E)
+  Note the 1/Q dependence which is used to simulate the Q-dependence of the signal for a normalized
+  empty can in BASIS at room temperature
+
+  Arguments:
+    model: beamline model file is a single line, e.g,
+           b0=1.3211; b1=0.00 e0=0.09; c0=2.3
+    resolution: Nexus file containing the resolution. This will be used to produce a elastic line.
+    convolved: Nexus file containing the convolution of the simulated S(Q,E) with the resolution.
+    qvalues: single-column file containing list of Q-values
+    assembled: output Nexus file containing the assembled S(Q,E) of the beamline model and the simulated S(Q,E)
+    expdata: Optional, experimental nexus file. If passed, output convolved will be binned as expdata.
+    costfile: Optional, file to store cost. If passed, the cost of comparing convolved and expdata will be saved.
+
+  Returns:
+    workspace containing the assembled S(Q,E)
+  """
+  import numpy
+  from mantid.simpleapi import (LoadNexus, ScaleX, ConvertToPointData, SaveNexus, DakotaChiSquared)
+  Q=[float(q) for q in open(qvalues,'r').read().split('\n')]
+  p={}
+  for pair in open(model,'r').readline().split(';'):
+    key,val=pair.split('=')
+    p[key.strip()]=float(val.strip())
+  wsr=LoadNexus(Filename=resolution,OutputWorkspace='resolution')
+  wsr=ConvertToPointData(wsr)
+  E=wsr.readX(0)
+  wse=ScaleX(InputWorkspace=wsr, OutputWorkspace='elastic',factor=-1) # elastic line
+  wsc=LoadNexus(Filename=convolved,OutputWorkspace='convolved')
+  for i in range(wsc.getNumberHistograms()):
+    elastic=wse.readY(i) # elastic spectrum at a given Q
+    convolved=wsc.readY(i) # convolved spectrum at a given Q
+    wsc.setY(i, (p['b0']+p['b1']*E) + (1/Q[i]*p['e0']*elastic) + (p['c0']*convolved) ) # overwrite spectrum
+  SaveNexus(InputWorkspace=wsc, Filename=assembled)
+  if expdata and costfile:
+    DakotaChiSquared(DataFile=assembled,CalculatedFile=expdata,OutputFile=costfile)
+  return wsc
+
 def lowTResolution(model, simulated, resolution, convolved, expdata=None,
                    costfile=None, **kwargs):
   """Convolve a simulated S(Q,E) with a resolution file
