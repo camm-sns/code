a0fcc04ac336f65239ff3ff721d8f3d1050093bf
Vickie Lynch <lynchve@ornl.gov>
2013-07-15 09:01:12 -0400

    run_hopper.sh added

---
+++ run_hopper.sh
@@ -0,0 +1,6 @@
+rm params* results* assembly* fit* Q*psf *log output.csv dakota.rst nohup.out
+CAMM=/tmp/code
+cp $CAMM/dakota/dakota.in .
+cp $CAMM/dakota/opt* .
+nohup dakota dakota.in &
+/usr/local/kepler/kepler.sh -runwf -nogui $CAMM/kepler/Strategist.xml -LocalWorkingDirectory $PWD -LocalUserName $USER -RemoteLogin vlynch@hopper.nersc.gov -CammDirectory $CAMM/simulation/src -NAMDBatchScript $CAMM/simulation/namd_hopper.pbs -SASSENABatchScript $CAMM/simulation/sassina_hopper.pbs -RemoteWorkingDirectory /scratch/scratchdirs/vlynch -RemoteBatchPath /opt/torque/4.2.3.1/bin
