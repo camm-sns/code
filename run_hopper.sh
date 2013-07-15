c900a5775493140c3c2e90e26b0d1b4cda129ada
Vickie Lynch <lynchve@ornl.gov>
2013-07-15 10:03:41 -0400

    Use 3 separate batch jobs on hopper to shorten queue wait

--- run_hopper.sh
+++ run_hopper.sh
@@ -3,4 +3,4 @@ CAMM=/tmp/code
 cp $CAMM/dakota/dakota.in .
 cp $CAMM/dakota/opt* .
 nohup dakota dakota.in &
-/usr/local/kepler/kepler.sh -runwf -nogui $CAMM/kepler/Strategist.xml -LocalWorkingDirectory $PWD -LocalUserName $USER -RemoteLogin vlynch@hopper.nersc.gov -CammDirectory $CAMM/simulation/src -NAMDBatchScript $CAMM/simulation/namd_hopper.pbs -SASSENABatchScript $CAMM/simulation/sassina_hopper.pbs -RemoteWorkingDirectory /scratch/scratchdirs/vlynch -RemoteBatchPath /opt/torque/4.2.3.1/bin
+/usr/local/kepler/kepler.sh -runwf -nogui $CAMM/kepler/Strategist_1job.xml -LocalWorkingDirectory $PWD -LocalUserName $USER -RemoteLogin vlynch@hopper.nersc.gov -CammDirectory $CAMM/simulation/src -NAMDBatchScript $CAMM/simulation/namd_hopper_1job.pbs -SASSENABatchScript $CAMM/simulation/sassina_hopper_2jobs.pbs -RemoteWorkingDirectory /scratch/scratchdirs/vlynch -RemoteBatchPath /opt/torque/4.2.3.1/bin
