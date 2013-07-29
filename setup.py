890cd44ab49c049cb39535350bbd108c8e083731
Doucet, Mathieu <doucetm@ornl.gov>
2013-07-29 14:31:47 -0400

    tweak install

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@
 """
 from setuptools import setup
 import shutil
+import os
+import sys
+import site
 
 setup(
     name         = "camm_amq",
     version      = "1.1",
     description  = "ActiveMQ consumer for CAMM",
     author       = "Oak Ridge National Laboratory",
-    package_dir  = {'camm_amq': '.'},
+    package_dir  = {'camm_amq': 'activemq'},
     packages     = ['camm_amq'],
     entry_points = {'console_scripts':["kepler_client = camm_amq.kepler_utilities:run_kepler_client",
                                        "kepler_results_ready = camm_amq.kepler_utilities:send_amq_results_ready",
@@ -19,3 +22,8 @@ setup(
                                        "camm_status = camm_amq.camm_monitor:send_status_info_command",
                                        "camm_monitor = camm_amq.camm_monitor:run",]},
     )   
+
+
+fd = open(os.path.join(site.getsitepackages()[0], 'camm.pth'), 'w')
+fd.write('/usr/local/camm')
+fd.close()
