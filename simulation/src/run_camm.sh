b4674be6e323d11b252855f38d24f95fa0927e40
Vickie Lynch <vlynch@ornl.gov>
2013-08-23 14:51:47 -0400

    Refs #25 get PID from dakota in this workflow

--- simulation/src/run_camm.sh
+++ simulation/src/run_camm.sh
@@ -2,6 +2,7 @@ rm params* results* assembly* fit* Q*psf *log dakota.rst nohup.out
 pkill dakota
 pkill opt_driver
 pkill python
-CAMM=/usr/local/camm
+CAMM=/home/vel/camm-sns/code
 cp $CAMM/dakota/opt* .
-nohup dakota $1 &
+nohup dakota -i $1 -o dakota.log >/dev/null </dev/null  &
+echo $!
