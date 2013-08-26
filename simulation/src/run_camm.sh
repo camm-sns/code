5129507d33a6762d7956d4829fde82548e7e66e4
Vickie Lynch <lynchve@ornl.gov>
2013-08-26 11:50:18 -0400

    Refs #25 get PID from starting dakota to use with ActiveMQ

--- simulation/src/run_camm.sh
+++ simulation/src/run_camm.sh
@@ -2,7 +2,6 @@ rm params* results* assembly* fit* Q*psf *log dakota.rst nohup.out
 pkill dakota
 pkill opt_driver
 pkill python
-CAMM=/home/vel/camm-sns/code
-cp $CAMM/dakota/opt* .
-nohup dakota -i $1 -o dakota.log >/dev/null </dev/null  &
+cp $2/../../dakota/opt* .
+nohup dakota -i $1 -o dakota.log >/dev/null 2>/dev/null </dev/null  &
 echo $!
