c59a594da79bce6011df0751f818529ac59c2e7f
Vickie Lynch <lynchve@ornl.gov>
2013-10-30 09:14:58 -0400

    Refs #13 ties for parameters written to dakota.in

--- simulation/src/molmec/dakota/initfile.py
+++ simulation/src/molmec/dakota/initfile.py
@@ -12,11 +12,28 @@ def populate_variables(template,free_params):
   """
   names=[param._name for param in free_params]
   template=template.replace('_NVAR_',str(len(names)))
-  template=template.replace( '_DESCRIPTORS_', "\t"+"\t".join(sorted(names)) )
+  template=template.replace( '_DESCRIPTORS_', "\t"+"\'\t\'".join(sorted(names)) )
   order=sorted(range(len(names)), key = names.__getitem__) #python magic :)
   fields={'_init':'_INITIAL_POINT_',
           '_minimum':'_LOWER_BOUNDS_',
           '_maximum':'_UPPER_BOUNDS_',
+          '_tolerance':'_MAX_STEP_',
+          }
+  for attribute,field in fields.items():
+    values=[param.__getattribute__(attribute) for param in free_params]
+    template=template.replace(field, "\t"+"\t".join([str(values[i]) for i in order]))
+  return template
+
+def populate_fixed(template,free_params):
+  """Use the list of free parameters to substitute in the
+  'variables' section of the init file
+  Order alphabetically
+  """
+  names=[param._name for param in free_params]
+  template=template.replace('_NFIX_',str(len(names)))
+  template=template.replace( '_FIXDESCRIPTORS_', "\t"+"\t".join(sorted(names)) )
+  order=sorted(range(len(names)), key = names.__getitem__) #python magic :)
+  fields={'_tie':'_FIXINITIAL_POINT_',
           '_tolerance':'_MAX_STEP_',
           }
   for attribute,field in fields.items():
@@ -43,14 +60,14 @@ method,
           output debug
 
 variables,
-        continuous_state = 1
-          initial_state 0.0
-          descriptors  'b1'
-        continuous_design = 7
-          cdv_initial_point 7.261128286320040e-01 1.000000000000000e-08 5.509589221505974e-02 4.438857367328487e-02 3.659833517176896e-02 2.206616918284543e-04 4.170000000000000e-01
-          cdv_lower_bounds   0.25 1e-8 0.0 0.0 0.0 -4.e-4 0.3
-          cdv_upper_bounds   2.0 2e-4 0.1 0.1 0.1 4.e-4 0.5
-          cdv_descriptors    'c0' 'b0' 'e0.0' 'e0.1' 'e0.2' 'eshift' 'FF1'
+        continuous_state = _NFIX_
+          initial_state _FIXINITIAL_POINT_
+          descriptors  _FIXDESCRIPTORS_
+        continuous_design = _NVAR_
+          cdv_initial_point _INITIAL_POINT_
+          cdv_lower_bounds   _LOWER_BOUNDS_
+          cdv_upper_bounds   _UPPER_BOUNDS_
+          cdv_descriptors   _DESCRIPTORS_
 
 interface,
         fork
@@ -70,5 +87,7 @@ responses,
   params=loadFFtpl(args.fftpl)[0] #list of FFParam objects
   free_params=[param for param in params if param.isFree()]
   template=populate_variables(template,free_params)
+  free_params=[param for param in params if not param.isFree()]
+  template=populate_fixed(template,free_params)
 
   open(args.outf,'w').write(template)
